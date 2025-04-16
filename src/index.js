304d2a64da7d146e5e973bf518087b0045c2ec09
Marcos Gabriel Paz <117696460+Marcaogabriel@users.noreply.github.com>
2025-04-16 09:24:13 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client'; 
 import { RouterProvider ,createBrowserRouter} from 'react-router-dom';
 import Home from './pages/Home';
 import ErrorPage from './pages/Errorpage';
